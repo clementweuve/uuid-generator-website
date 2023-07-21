70be610ef36976ef45c84bc4221a8201308c2cea
clementweuve <140206419+clementweuve@users.noreply.github.com>
2023-07-21 20:39:56 +0000

    minor modifications

--- filemanager.py
+++ filemanager.py
@@ -3,18 +3,20 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 scheduler = BackgroundScheduler()
 
+
 def uuid_file_management():
     files = os.listdir("static/UUID_files/")
-    if files!=None:
+    if files != None:
         for file in files:
             os.remove(f'static/UUID_files/{file}')
 
 
-def create_uuid_file(filepath:str, uuid_list:list):
+def create_uuid_file(filepath: str, uuid_list: list):
     file = os.open(filepath, "w+")
     for uuid in uuid_list:
-        file.write(f"{uuid} \n" )
+        file.write(f"{uuid} \n")
     file.close()
+
 
 scheduler.add_job(uuid_file_management, 'interval', seconds=120)
 scheduler.start()
